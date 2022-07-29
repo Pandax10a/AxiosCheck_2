7f0d1806daef74c648b0a4db22018795b8ae7cc5
panda1 <blueblue0011@gmail.com>
2022-07-28 18:12:51 -0700

    need to make image show up:

--- app.js
+++ app.js
@@ -14,3 +14,25 @@ function failureFunction(error333333333) {
         document.body.insertAdjacentHTML(`beforeend`, `<h1> failed</h1>`);
     
 }
+
+axios.request({
+    url: `https://www.themealdb.com/api/json/v1/1/filter.php`,
+    params: {
+        c: `Chicken`
+    }
+}).then(successFunction2).catch(failureFunction2);
+
+// if the api successfully responds, it will take the object inside data array and print the activity
+function successFunction2(response222) {
+
+    for(let i = 0; i <response222[`data`][`meals`].length; i++){
+   
+    document.body.insertAdjacentHTML(`beforeend`, `<h3>${response222[`data`][`meals`][i][`strMeal`]}</h3> 
+    <img  src = "${response222[`data`][`meals`][i][`strMealThumb`]}">`);
+}
+}
+// just in case it fails to receive data from api
+function failureFunction2(error333333333) {
+        document.body.insertAdjacentHTML(`beforeend`, `<h1> failed</h1>`);
+    
+}
